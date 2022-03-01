4beabcad5e82088f1151bbd594c9541a5be6c6f7
Vidinho <gabrielvido9@gmail.com>
2022-03-01 19:30:40 -0300

    Exercicio de Temperatura pra treina

--- src/Temperaturas.java
+++ src/Temperaturas.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Temperaturas {
     public static void main(String[] args) {
+        double kelvin, reaumur, rankine, fahrenheit;
+        Scanner leitor = new Scanner(System.in);
 
+        System.out.println("Digite a temperatura");
+        Double temperatura = leitor.nextDouble();
+
+        kelvin = (temperatura + 273.15);
+        System.out.println("A temperatura em Kelvin é "+kelvin);
+
+        reaumur = temperatura * 0.8;
+        System.out.println("A temperatura em Reaumur é "+reaumur);
+
+        rankine = temperatura * 1.8 + 32 + 459.67;
+        System.out.println("A temperatura em Rankine é "+rankine);
+
+        fahrenheit = temperatura * 1.8 + 32;
+        System.out.println("A temperatura em Fahrenheit é "+fahrenheit);
     }
 }
